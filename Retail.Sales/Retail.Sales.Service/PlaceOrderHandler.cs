dd0a9777a274a17f483cc14f365d2ec38823c03b
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2020-05-28 21:07:00 +0200

    Add Retail.Sales console app

---
+++ Retail.Sales/Retail.Sales.Service/PlaceOrderHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Retail.Sales.Service
+{
+    using System.Threading.Tasks;
+    using Frontend.Messages;
+    using NServiceBus;
+    using NServiceBus.Logging;
+
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        private readonly ILog log;
+
+        public PlaceOrderHandler(ILog log)
+        {
+            this.log = log;
+        }
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            this.log.Info($"Order {message.OrderId} received!");
+            return Task.CompletedTask;
+        }
+    }
+}
+
+
---
+++ Retail.Sales/Retail.Sales.Service/Program.cs
@@ -0,0 +1,33 @@
+﻿using NServiceBus.Logging;
+
+namespace Retail.Sales.Service
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using NServiceBus;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Sales Service";
+
+            var endpointConfiguration = new EndpointConfiguration("sales");
+            var containerSettings = endpointConfiguration.UseContainer(new DefaultServiceProviderFactory());
+            containerSettings.ServiceCollection.AddSingleton(LogManager.GetLogger("Default"));
+
+            endpointConfiguration
+                .UseTransport<RabbitMQTransport>()
+                .UseDirectRoutingTopology()
+                .ConnectionString("host=localhost");
+
+            var endpoint = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            await endpoint.Stop()
+                .ConfigureAwait(false);
+        }
+    }
+}
