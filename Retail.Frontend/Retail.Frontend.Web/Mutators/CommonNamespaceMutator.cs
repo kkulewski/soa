ccb28af0bc9fe36906303e943278488c634dcc75
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2020-06-04 23:50:33 +0200

    Add outgoing tranport mutator

---
+++ Retail.Frontend/Retail.Frontend.Web/Mutators/CommonNamespaceMutator.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.MessageMutator;
+
+namespace Retail.Frontend.Web.Mutators
+{
+    public class CommonNamespaceMutator : IMutateOutgoingTransportMessages
+    {
+        public Task MutateOutgoing(MutateOutgoingTransportMessageContext context)
+        {
+            var headers = context.OutgoingHeaders;
+            headers["NServiceBus.EnclosedMessageTypes"] = $"Retail.{context.OutgoingMessage.GetType().Name}";
+            return Task.CompletedTask;
+        }
+    }
+}
