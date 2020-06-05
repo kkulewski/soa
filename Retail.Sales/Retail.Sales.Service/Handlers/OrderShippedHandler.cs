fb70b84463788bdbc9a191304f61d66848cb94b5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2020-06-05 03:17:23 +0200

    Handle order shipped event in sales service

---
+++ Retail.Sales/Retail.Sales.Service/Handlers/OrderShippedHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Retail.Sales.Service.Handlers
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using Messages;
+
+    public class OrderShippedHandler : IHandleMessages<OrderShipped>
+    {
+        private readonly ILog log;
+
+        public OrderShippedHandler(ILog log)
+        {
+            this.log = log;
+        }
+
+        public Task Handle(OrderShipped message, IMessageHandlerContext context)
+        {
+            this.log.Info($"Order {message.OrderId} processing finished.");
+            return Task.CompletedTask;
+        }
+    }
+}
+
+
---
+++ Retail.Sales/Retail.Sales.Service/Messages/OrderShipped.cs
@@ -0,0 +1,9 @@
+﻿namespace Retail.Sales.Service.Messages
+{
+    using NServiceBus;
+
+    public class OrderShipped : IEvent
+    {
+        public string OrderId { get; set; }
+    }
+}
