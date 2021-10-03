2b0723881c19032c58f06d61517923ccb107cabb
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2021-10-03 23:34:42 +0200

    Add Svelte frontend to product catalog

---
+++ Retail.Catalog.Frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let products;
+
+	async function getProducts () {
+		const res = await fetch('http://localhost:5001/product', { method: 'GET'} )
+		products = await res.json()
+	}
+
+</script>
+
+<button type="button" on:click={getProducts}>Fetch products</button>
+<ul>
+	{#each products as product}
+		<li>[{product.productId}] {product.name} - {product.description}</li>
+	{/each}
+</ul>
+
+<style>
+</style>
