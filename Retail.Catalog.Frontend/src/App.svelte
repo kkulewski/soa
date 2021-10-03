f812c85fbfa8294b7b1eb76a1de779ff66a4c526
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2021-10-04 00:10:16 +0200

    Add product insert form to catalog service

--- Retail.Catalog.Frontend/src/App.svelte
+++ Retail.Catalog.Frontend/src/App.svelte
@@ -6,14 +6,47 @@
 		products = await res.json()
 	}
 
+	let newProduct = {
+		productId: null,
+		name: null,
+		description: null
+	};
+
+	let addProductResult = {
+		status: null,
+		statusText: null
+	}
+
+	async function addProduct() {
+		const res = await fetch('http://localhost:5001/product', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(newProduct)
+		})
+
+		addProductResult = await res;
+	}
+
 </script>
 
-<button type="button" on:click={getProducts}>Fetch products</button>
+<h1>Product list</h1>
+
+<button type="button" on:click={getProducts}>Fetch</button>
 <ul>
 	{#each products as product}
 		<li>[{product.productId}] {product.name} - {product.description}</li>
 	{/each}
 </ul>
+
+<hr/>
+
+<h1>Add product</h1>
+
+<label for="productId">ID:</label><input bind:value={newProduct.productId} name="productId" /><br/>
+<label for="productName">Name:</label><input bind:value={newProduct.name} name="productName" /><br/>
+<label for="productDescription">Description:</label><input bind:value={newProduct.description} name="productDescription" /><br/>
+<button type="button" on:click={addProduct}>Add</button><br/>
+<p>Result: {addProductResult.status} - {addProductResult.statusText}</p>
 
 <style>
 </style>
