214c30e11f85ada79afb56657bbfe25f0c92ad5c
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2022-05-06 23:22:11 +0200

    Remove unused libs

--- Retail.Frontend/src/App.vue
+++ Retail.Frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
@@ -8,26 +8,23 @@ import HelloWorld from "@/components/HelloWorld.vue";
     <img
       alt="Vue logo"
       class="logo"
-      src="@/assets/logo.svg"
+      src="./assets/logo.svg"
       width="125"
       height="125"
     />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
 <style>
-@import "@/assets/base.css";
+@import "./assets/base.css";
 
 #app {
   max-width: 1280px;
@@ -39,7 +36,6 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 header {
   line-height: 1.5;
-  max-height: 100vh;
 }
 
 .logo {
@@ -58,31 +54,6 @@ a,
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
   }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -111,15 +82,6 @@ nav a:first-of-type {
 
   .logo {
     margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
