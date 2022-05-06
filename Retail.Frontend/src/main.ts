552c82bd27ac7910c0917849e728b4c138a5c91a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2022-05-07 01:27:05 +0200

    Copy OIDC client to host container

--- Retail.Frontend/src/main.ts
+++ Retail.Frontend/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+
+Vue.config.ignoredElements = ["sales-buy"];
 
 createApp(App).mount("#app");
