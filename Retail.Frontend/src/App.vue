5ebffe7ed95051ea7eb39d565ac44e4d5a1bbd99
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2022-05-06 23:07:15 +0200

    Convert frontend to Vue3 project

--- Retail.Frontend/src/App.vue
+++ Retail.Frontend/src/App.vue
@@ -1,70 +1,125 @@
-<template>
-  <div id="app">
-
-    <header>
-      <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-          <div class="container">
-              <a class="navbar-brand" asp-area="" asp-controller="Home" asp-action="Index">Retail</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
-                  <ul class="navbar-nav flex-grow-1">
-                      <li class="nav-item">
-                          <a class="nav-link text-dark">Home</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link text-dark">Privacy</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
-    </header>
-
-    <div class="container">
-      <Home/>
-    </div>
-
-    <br/>
-
-    <div class="border-top"></div>
-
-    <br/>
-
-    <div class="container">
-      <Products msg="Retail"/>
-    </div>
-
-    <footer class="border-top footer text-muted">
-      <br/>
-      <div class="container">
-        &copy; 2021 - Retail
-      </div>
-    </footer>
-
-  </div>
-</template>
-
-<script>
-import Products from './components/Products.vue'
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Products,
-    Home
-  }
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
 <style>
+@import "@/assets/base.css";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
   text-align: center;
-  color: #2c3e50;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
